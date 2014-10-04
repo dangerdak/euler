4b31db5e160077d78d51b93d3c3334e951d8a4c3
dangerdak <dakshina.scott@gmail.com>
2014-10-04 21:30:12 +0100

    MUCH faster

--- prob3.py
+++ prob3.py
@@ -1,26 +1,17 @@
 from sys import argv
 
 
-def factors(n):
-    """Returns a list of factors of n, excluding 1 and itself."""
-    factors = [i for i in range(2, n) if n % i == 0]
-    return factors
-
-
-def largest_prime(l):
-    """Returns the largest prime from a list of numbers l."""
-    # Sort number from largest to smallest
-    l.sort(reverse=True)
-    for number in l:
-        # If number has no factors then it's a prime
-        if len(factors(number)) == 0:
-            return number
+def greatest_prime_factor(n):
+    """Returns greatest prime factor of n. If n is prime, returns n."""
+    divisor = 2
+    while divisor < n:
+        if n % divisor == 0:
+            n = int(n / divisor)
+            divisor = 1
+        # Python doesn't have an increment operator!
+        divisor += 1
+    return n
 
 
 if __name__ == '__main__':
-
-    n = int(argv[1])
-
-    factors_list = factors(n)
-    print(factors_list)
-    print(largest_prime(factors_list))
+    print(greatest_prime_factor(int(argv[1])))
