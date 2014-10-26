ddd3497a4e01aa8de75a86de132d3848e243b4a7
dangerdak <dakshina.scott@gmail.com>
2014-10-26 22:08:00 +0000

    Add kwargs

--- utils.py
+++ utils.py
@@ -2,9 +2,9 @@ import time
 
 
 def timed(func):
-    def timer(*args):
+    def timer(*args, **kwargs):
         t0 = time.time()
-        result = func(*args)
+        result = func(*args, **kwargs)
         t1 = time.time()
         runtime = t1-t0
         print('Runtime:', runtime)
